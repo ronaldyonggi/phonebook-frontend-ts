1188f975c1744c9a4170e06fd8e075c715b4edcf
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-01-22 21:03:43 +0800

    update initial persons with additional data

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import IndividualPerson from "./components/IndividualPerson"
 import { Person } from "./types/Person"
 
 const App = () => {
-  const [persons, setPersons] = useState<Person[]>([
-    {
-      id: 1,
-      name: 'Arto Hellas',
-      number: '123-4567'
-    }
-  ])
+  const initialPersons = [
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]
+
+  const [persons, setPersons] = useState<Person[]>(initialPersons)
   return (
     <div>
       <h2>Phonebook</h2>
