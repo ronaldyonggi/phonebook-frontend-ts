8a04133b11bbfd5cb98feb234dc76975322f9d9d
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-01-22 19:34:05 +0800

    2.6 initial app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
+import { useState } from "react"
+import AddPersonForm from "./components/AddPersonForm"
+import IndividualPerson from "./components/IndividualPerson"
+import { Person } from "./types/Person"
 
-function App() {
-
+const App = () => {
+  const [persons, setPersons] = useState<Person[]>([
+    {
+      id: 1,
+      name: 'Arto Hellas',
+      number: '123-4567'
+    }
+  ])
   return (
-    <>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <AddPersonForm persons={persons} setPersons={setPersons} />
+      <h2>Numbers</h2>
+      {persons.map(person => (
+        <IndividualPerson key={person.id} person={person} />
+      ))}
+    </div>
   )
 }
-
 export default App
