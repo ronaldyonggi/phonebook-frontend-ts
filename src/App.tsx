36bd37bc2a695f9e0eafb2eee82841c899086e4d
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-01-23 10:01:35 +0800

    2.11 fetch initial state of app from db.json via axios GET

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import AddPersonForm from "./components/AddPersonForm"
 import IndividualPerson from "./components/IndividualPerson"
 import { Person } from "./types/Person"
+import axios from "axios"
 
 const App = () => {
-  const initialPersons = [
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]
-
-  const [persons, setPersons] = useState<Person[]>(initialPersons)
+  const [persons, setPersons] = useState<Person[]>([])
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(res => setPersons(res.data))
+  }, [])
 
   const personsToShow = filter
   ? persons.filter(person => person.name.toLowerCase().includes(filter) )
