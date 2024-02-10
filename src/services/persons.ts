49799dd2857fa797339d547546408c30db9fa9d0
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-02-10 13:37:32 +0800

    requests are handled using axios

--- src/services/persons.ts
+++ src/services/persons.ts
@@ -1,27 +1,27 @@
-import axios from "axios"
-import { Person } from "../types/Person"
+import axios from 'axios';
+import { Person } from '../types/person';
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons';
 
 const getAll = () => {
-  return axios.get<Person[]>(baseUrl)
-}
+  return axios.get<Person[]>(baseUrl);
+};
 
 const create = (newObject: object) => {
-  return axios.post<Person>(baseUrl, newObject)
-}
+  return axios.post<Person>(baseUrl, newObject);
+};
 
 const update = (id: string, newObject: object) => {
-  return axios.put<Person>(`${baseUrl}/${id}`, newObject)
-}
+  return axios.put<Person>(`${baseUrl}/${id}`, newObject);
+};
 
 const deletePerson = (id: string) => {
-  return axios.delete<Person>(`${baseUrl}/${id}`)
-}
+  return axios.delete<Person>(`${baseUrl}/${id}`);
+};
 
 export default {
   getAll,
   create,
   update,
-  deletePerson
-}
+  deletePerson,
+};
